2419fcfe0a7ca0656a3893f240e530eebb23239e
Kevin Deisz <kevin.deisz@gmail.com>
2018-12-03 13:57:40 -0500

    More fully test out pie chart

--- setupTests.js
+++ setupTests.js
@@ -1,2 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import "react-testing-library/cleanup-after-each";
+
+class XMLSerializer {
+  serializeToString(svg) {
+    return "svg";
+  }
+}
+
+window.XMLSerializer = XMLSerializer;
--- src/__tests__/PieChart.test.js
+++ src/__tests__/PieChart.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "react-testing-library";
+import { render, fireEvent, waitForElement } from "react-testing-library";
 
 import PieChart from "../PieChart";
 
@@ -25,4 +25,11 @@ test("syncs with data", () => {
   rerender(<PieChart data={data} />);
 
   expect(container.querySelectorAll(".chq-charts--pie-slice")).toHaveLength(4);
+});
+
+test("finishes animation and cancels interval", async () => {
+  const { getByText } = render(<PieChart data={{ a: 10, b: 0 }} />);
+
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  await waitForElement(() => getByText("100% (10)"));
 });
