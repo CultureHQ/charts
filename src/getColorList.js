21ed7b9a9a49dbae29c5e5226b8571129ccc9f11
Kevin Deisz <kevin.deisz@gmail.com>
2018-12-03 13:23:46 -0500

    Use hsl values for better colors

--- src/getColorList.js
+++ src/getColorList.js
@@ -1,13 +1,10 @@
 const STATIC_COLORS = ["#8cb4d6", "#79b17d", "#ffd24b"];
 
-const getRandomValue = () => Math.floor(Math.random() * 255);
-const getRandomColor = () => `rgb(${getRandomValue()}, ${getRandomValue()}, ${getRandomValue()})`;
-
 const getColorList = size => {
   const colors = [...STATIC_COLORS];
 
   for (let idx = 3; idx < size; idx += 1) {
-    colors.push(getRandomColor());
+    colors.push(`hsl(${Math.round(Math.random() * 360)}, 33%, 66%)`);
   }
 
   return colors.slice(0, size);
