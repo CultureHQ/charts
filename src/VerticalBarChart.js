dffc57adaed180cf58af0db5b5a698068a8e8172
Kevin Deisz <kevin.deisz@gmail.com>
2018-12-03 11:13:26 -0500

    Use Chart and ChartSegment for VerticalBarChart

--- src/VerticalBarChart.js
+++ src/VerticalBarChart.js
@@ -1,73 +1,123 @@
-import React, { memo } from "react";
+import React, { PureComponent } from "react";
 
 import getColorList from "./getColorList";
+import Chart from "./Chart";
+import ChartSegment from "./ChartSegment";
 
-const VerticalBarChart = memo(({ data }) => {
+const makeChartConfig = data => {
   const keys = Object.keys(data);
   const colors = getColorList(keys.length);
 
   const maxValue = Math.max(...Object.values(data));
   const maxX = keys.length * 40;
 
-  return (
-    <svg
-      className="chq-charts--chart chq-charts--vert-bar"
-      viewBox={`0 0 ${maxX} 325`}
-    >
-      {Object.keys(data).map((key, index) => {
-        const height = (data[key] / maxValue) * 250;
+  return { keys, colors, maxValue, maxX };
+};
 
-        return (
-          <g key={key}>
-            <text
-              x={index * 40} y={305}
-              textAnchor="middle"
-              transform={`rotate(-30, ${index * 40}, 305)`}
-            >
-              {key}
-            </text>
-            <text
-              x={index * 40 + (25 / 2)} y={265 - height}
-              textAnchor="middle"
-            >
-              {data[key]}
-            </text>
-            {height !== 0 && (
-              <g className="chq-charts--vert-bar-group" tabIndex={0}>
-                <rect
-                  width={25}
-                  height={height}
-                  x={index * 40} y={275 - height}
-                  fill={colors[index]}
-                />
-                <rect
-                  className="chq-charts--bar-shadow"
-                  width={35}
-                  height={height + 5}
-                  x={index * 40 - 5} y={270 - height}
-                  fill={colors[index]}
-                />
-              </g>
-            )}
-            {index !== keys.length - 1 && (
-              <line
-              x1={(index + 1) * 40 - 7.5} y1={285}
-                x2={(index + 1) * 40 - 7.5} y2={265}
-                stroke="#ccc"
-                strokeWidth={1}
-              />
-            )}
-          </g>
-        );
-      })}
-      <line
-        x1={-10} y1={280}
-        x2={maxX} y2={280}
-        stroke="#ccc"
-        strokeWidth={1}
-      />
-    </svg>
+const ChartBarGroup = ({ dataKey, dataValue, maxValue, index, color, isLast, tabIndex, onClick, onKeyDown }) => {
+  const height = (dataValue / maxValue) * 250;
+
+  return (
+    <g>
+      <text
+        x={index * 40} y={305}
+        textAnchor="middle"
+        transform={`rotate(-30, ${index * 40}, 305)`}
+      >
+        {dataKey}
+      </text>
+      <text
+        x={index * 40 + (25 / 2)} y={265 - height}
+        textAnchor="middle"
+      >
+        {dataValue}
+      </text>
+      {height !== 0 && (
+        <g
+          className="chq-charts--vert-bar-group"
+          tabIndex={tabIndex}
+          onClick={onClick}
+          onKeyDown={onKeyDown}
+        >
+          <rect
+            width={25}
+            height={height}
+            x={index * 40} y={275 - height}
+            fill={color}
+          />
+          <rect
+            className="chq-charts--bar-shadow"
+            width={35}
+            height={height + 5}
+            x={index * 40 - 5} y={270 - height}
+            fill={color}
+          />
+        </g>
+      )}
+      {!isLast && (
+        <line
+          x1={(index + 1) * 40 - 7.5} y1={285}
+          x2={(index + 1) * 40 - 7.5} y2={265}
+          stroke="#ccc"
+          strokeWidth={1}
+        />
+      )}
+    </g>
   );
-});
+};
+
+const ChartBar = props => <ChartSegment component={ChartBarGroup} {...props} />;
+
+class ChartSVG extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = { chartConfig: makeChartConfig(props.data) };
+  }
+
+  componentDidUpdate(prevProps) {
+    const { data } = this.props;
+
+    if (data !== prevProps.data) {
+      this.setState({ chartConfig: makeChartConfig(data) });
+    }
+  }
+
+  render() {
+    const { data, onToggle, onDeselect } = this.props;
+    const { chartConfig } = this.state;
+
+    const { keys, colors, maxValue, maxX } = chartConfig;
+
+    return (
+      <svg
+        className="chq-charts--chart chq-charts--vert-bar"
+        viewBox={`0 0 ${maxX} 325`}
+      >
+        {keys.map((key, index) => (
+          <ChartBar
+            key={key}
+            dataKey={key}
+            dataValue={data[key]}
+            maxValue={maxValue}
+            index={index}
+            color={colors[index]}
+            isLast={index === keys.length - 1}
+            onToggle={onToggle}
+            onDeselect={onDeselect}
+          />
+        ))}
+        <line
+          x1={-10} y1={280}
+          x2={maxX} y2={280}
+          stroke="#ccc"
+          strokeWidth={1}
+        />
+      </svg>
+    );
+  }
+}
+
+const VerticalBarChart = ({ data }) => <Chart component={ChartSVG} data={data} />;
 
 export default VerticalBarChart;
