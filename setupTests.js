f6d8231d8ccbd0b336894a2be6c75d7a49b34ef7
Kevin Deisz <kevin.deisz@gmail.com>
2019-04-04 11:38:19 -0400

    Switch to using @culturehq/scripts for development

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import "react-testing-library/cleanup-after-each";
-
 class XMLSerializer {
   // eslint-disable-next-line class-methods-use-this
   serializeToString() {
